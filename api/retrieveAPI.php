84c626e52e120fd5c742ebcee4ed35df9fe4baa1
NKTeo <51158551+NKTeo@users.noreply.github.com>
2020-10-20 22:19:28 +0800

    Update retrieveAPI.php

--- api/retrieveAPI.php
+++ api/retrieveAPI.php
@@ -1,42 +1,55 @@
 <?php
 
 // create & initialize a curl session
-$curl = curl_init();
+function call_api($url) {
+    $curl = curl_init();
+    
+    
+    // set our url with curl_setopt()
+    curl_setopt($curl, CURLOPT_URL,$url );
+    
+    // Set cURL Headers
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'AccessKey: 31d76d00-70aa-46bc-b5d0-5e025149ac5a',
+      'Token: w4eVdfDgYWc15e6PU7ZUKNB7dX0d16-gnbQ-xddrpD@F5MaZ6Ecc2aabHr5@20BhF0sDS-m4AVc--5gcs23Xcda0y35MnExja56Y',
+    ));
+    
+    // return the transfer as a string, also with setopt()
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-// URL to send the request to
-curl_setopt($curl, CURLOPT_URL, "http://jsonplaceholder.typicode.com/users");
+    // To prevent Curl error: SSL certificate problem: unable to get local issuer certificate
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 
-// Return the transfer as a string
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // if(curl_exec($curl) === false)
+    // {
+    //     echo 'Curl error: ' . curl_error($curl);
+    // }
+    // else
+    // {
+    //     echo 'Operation completed without any errors';
+    // }
 
-// Executes the started curl session
-// $json_output contains the JSON objects
-$json_output = curl_exec($curl);
+  
+    // curl_exec() executes the started curl session
+    // $output contains the output string
+    $output = curl_exec($curl);
+    
 
-// Check for errors
-if ($error = curl_error($curl)){
-  echo $error;
-} else {
+    // close curl resource to free up system resources
+    // (deletes the variable made by curl_init)
+    curl_close($curl);
 
-  // Displaying raw JSON output
-  // print_r($json_output);
+    $out_assoc = json_decode($output);
+    
+    echo gettype($out_assoc);
+    
+    return $output;
 
-  // json_decode true sets the output to an array
-  $json_decoded = json_decode($json_output, true);
-  // var_dump($json_decoded);
-
-  foreach($json_decoded as $decoded_item){
-    echo $decoded_item['name'];
-    echo "<br>";
-  }
 }
 
-// close curl resource to free up system resources
-// (deletes the variable made by curl_init)
-curl_close($curl);
+$url = "https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Availability";
 
-
-
-
+echo call_api($url);
 
 ?>
