f0c80f3191bf3e5403c157c1246c0cc252bea316
jiale-2019 <ong1jia1le@gmail.com>
2020-10-21 20:26:55 +0800

    added var that is an arr of the result

--- api/carpark/read.php
+++ api/carpark/read.php
@@ -1,12 +1,16 @@
 <?php
 require_once 'ura.php';
 
+// $ura_details is a json, details_arr is an array of all the results
 $url_details = "https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Details";
 $ura_details = call_ura_api($url_details);
+$details_arr = $ura_details['Result'];
 //var_dump($carpark_details);
 
+// $ura_avails is a json, avails_arr is an array of all the results
 $url_avails = "https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Details";
 $ura_avails = call_ura_api($url_avails);
+$avails_arr = $ura_avails['Result'];
 //var_dump($carpark_details);
 
 // required headers
