175973656cbf8602c2735da1983e1e45b77cd726
jiale-2019 <ong1jia1le@gmail.com>
2020-11-14 13:56:38 +0800

    Update search.php

--- api/carpark/search.php
+++ api/carpark/search.php
@@ -8,29 +8,41 @@ require_once 'getHDBnearest.php';
 $lat = isset($_GET["lat"]) ? $_GET["lat"] : "";
 $lng = isset($_GET["lng"]) ? $_GET["lng"] : "";
 $results = [];
-$results["HDB"] = getNearestHDBCP($lat, $lng);
-$results["URA"] =  getNearestURACP($lat, $lng);
-// var_dump($results);
 
-if (count($results) > 0) {
-    // set response code - 200 OK
-    http_response_code(200);
-
-    // show products data
-    echo json_encode($results);
-} 
-else {
-    // set response code - 404 Not found
+if ($lat == "" || $lng == "") {
     http_response_code(404);
 
     // tell the user no items found
     echo json_encode(
-        array("message" => "No items found.")
+        array("message" => "Invalid Destination Supplied")
     );
+} else {
+
+    $results["HDB"] = getNearestHDBCP($lat, $lng);
+    $results["URA"] =  getNearestURACP($lat, $lng);
+    // var_dump($results);
+
+
+
+    if (count($results) > 0) {
+        // set response code - 200 OK
+        http_response_code(200);
+
+        // show products data
+        echo json_encode($results);
+    } 
+    else {
+        // set response code - 404 Not found
+        http_response_code(404);
+
+        // tell the user no items found
+        echo json_encode(
+            array("message" => "No items found.")
+        );
+    }
+
+    // // required headers
+    // // header("Access-Control-Allow-Origin: *");
+    // // header("Content-Type: application/json; charset=UTF-8");
 }
-
-// // required headers
-// // header("Access-Control-Allow-Origin: *");
-// // header("Content-Type: application/json; charset=UTF-8");
-
 ?>
