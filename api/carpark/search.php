28122040edc42aefb73ec8202e58f1608f035fe2
jiale-2019 <ong1jia1le@gmail.com>
2020-11-15 16:08:21 +0800

    Update search.php

--- api/carpark/search.php
+++ api/carpark/search.php
@@ -37,18 +37,6 @@ if ($lat == "" || $lng == "") {
         // show products data
         echo json_encode($results);
     } 
-    else {
-        // set response code - 404 Not found
-        http_response_code(404);
 
-        // tell the user no items found
-        echo json_encode(
-            array("message" => "No items found.")
-        );
-    }
-
-    // // required headers
-    // // header("Access-Control-Allow-Origin: *");
-    // // header("Content-Type: application/json; charset=UTF-8");
 }
 ?>
